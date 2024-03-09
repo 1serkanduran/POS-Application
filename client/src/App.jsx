fe0bee26f8d951ac9920ad5d8f75c38d143b4f6e
Serkan Duran <75946630+1serkanduran@users.noreply.github.com>
2024-03-10 02:15:56 +0300

    UI design start and first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import Header from './components/header/Header';
+import Categories from './components/categories/Categories';
+import Products from './components/products/Products';
+import CartTotals from './components/cart/CartTotals';
+
+function App() {
+  return (
+      <>
+      <Header/>
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+          <Categories/>
+        </div>
+        <div className="products flex-[8]">
+          <Products/>
+        </div>
+        <div className="card-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <CartTotals/>
+        </div>
+      </div>
+      </>
+  );
+}
+
+export default App;
---
+++ client/src/components/products/Products.jsx
@@ -0,0 +1,130 @@
+
+
+const Products = () => {
+  return (
+    <div className="products-wrapper grid grid-cols-card gap-4">
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-image">
+                <img src="https://www.diyetkolik.com/site_media/media/customvideo_images/Yesil_elma_yag_yakar_m__Krmz_elma_m_yesil_elma_m__1_1.jpg"
+                 alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>₺</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Products
